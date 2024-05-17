b79e87e791130f5812e760a80859066c12e72ff0
Orfeu o Escarlate <darkarontg@gmail.com>
2024-05-17 14:21:23 -0300

    i almost fucked the project, but its alright i guess

--- convert_dbf_to_db.py
+++ convert_dbf_to_db.py
@@ -28,14 +28,47 @@ dbf_db_files = [f for f in os.listdir(dbf_directory) if f.endswith('.dbf')]
 # Lista o nome dos arquivos CSV na pasta auxiliar
 aux_db_files = [f for f in os.listdir(dbf_directory+"/aux") if f.endswith('.csv')]
 
+# Contador pra ver quantos arquivos foram convertidos
+counter = 0
+# Tempo inicial para pegar o tempo total que ficou convertendo e importando
+total_time = time.time()
 # Itera por todos os arquivos CSV
 for csv_name in aux_db_files:
+    counter += 1
     # pega o nome do arquivo sem extensão
     name = csv_name[:-4]
     # cria a tabela para o cnv que é um csv agora, e coloca o Código como chave primária
     database.sql(f"CREATE TABLE {name.lower()} (ID integer, descricao TEXT, Codigo Text, Primary Key (Codigo));")
     # Importa os dados pra dentro da tabela
     database.sql(f"INSERT INTO {name.lower()} SELECT * FROM read_csv('{dbf_directory}aux/{csv_name}');")
+    print(f"\r{counter}/{len(aux_db_files)} {round((counter/len(aux_db_files))*100, 2)}%",end="",)
+print(f"\nTempo total para inserção de todas os dados no BD {round(time.time() - total_time, 2)} segundos")
+
+aux_db_files = [f for f in os.listdir(dbf_directory+"/aux") if f.lower().endswith('.dbf')]
+
+# Contador pra ver quantos arquivos foram convertidos
+counter = 0
+# Tempo inicial para pegar o tempo total que ficou convertendo e importando
+total_time = time.time()
+
+for csv_name in aux_db_files:
+    counter += 1
+    # pega o nome do arquivo sem extensão
+    name = csv_name[:-4]
+    table = DBF(f'{dbf_directory}aux/{csv_name}', 'latin-1')
+    csv_path = f"{output_directory}/{name}.csv"
+    # Abre um CSV e joga todo as coisas do DBF para o CSV
+    with open(csv_path, "w", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(table.field_names)
+        for record in table:
+            writer.writerow(list(record.values()))
+    # cria a tabela para o cnv que é um csv agora, e coloca o Código como chave primária
+    database.sql(f"CREATE TABLE {name.lower()} (codigo Text, descr Text,  Primary Key (codigo));")
+    # Importa os dados pra dentro da tabela
+    database.sql(f"INSERT INTO {name.lower()} SELECT cd_cod, cd_descr FROM read_csv('{output_directory}{name}.csv');")
+    print(f"\r{counter}/{len(aux_db_files)} {round((counter/len(aux_db_files))*100, 2)}%",end="",)
+print(f"\nTempo total para inserção de todas os dados no BD {round(time.time() - total_time, 2)} segundos")
 
 # Abre o arquivo SQL da criação da tabela principal dos DBFs
 fd = open('database_creation.sql', 'r')
@@ -52,6 +85,7 @@ counter = 0
 # Tempo inicial para pegar o tempo total que ficou convertendo e importando
 total_time = time.time()
 for dbf_name in dbf_db_files:
+    start_time = time.time()
     counter += 1
     # Abre o DBF e joga pra um arquivo
     table = DBF(f'{dbf_directory}/{dbf_name}', 'latin-1')
@@ -71,8 +105,10 @@ for dbf_name in dbf_db_files:
     # Deleta o arquivo CSV criado
     os.remove(csv_path)
     # Mostra o progresso
-    print(f"\r{counter}/{len(dbf_db_files)} {round((counter/len(dbf_db_files))*100, 2)}%",end="",)
+    print(f"\r{counter}/{len(dbf_db_files)} {round((counter/len(dbf_db_files))*100, 2)}%   ----  Tempo demorado no ultimo arquivo: {round(time.time() - start_time, 2)} segundos",end="",)
 # Adicionado todos os DBFs na tabela
 database.close()
 # Mostra o tempo total de importação
-print(f"\nTempo total para inserção de todas os dados no BD {round(time.time() - total_time, 2)} segundos")
+total_time = time.time() - total_time
+total_time = f"{round(total_time, 2)} segundos" if total_time < 60 else f"{total_time/60} minutos"
+print(f"\nTempo total para inserção de todas os dados no BD {total_time}")
