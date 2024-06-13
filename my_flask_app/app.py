75faf81bb60addfa2ed2a2c41e99dc6a42a25ffb
Juan Fricke <juanpfricke@outlook.com>
2024-06-12 21:10:05 -0300

    initial setup with sass

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template
 
+import subprocess
+
+# Executa o comando 'ls -l'
+subprocess.run(['sass', 'static/bulma/bulma.scss:static/css/style.css'])
+
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', css_file='style.css')
 
-@app.route('/about')
+
+@app.route('/sobre')
 def about():
     return render_template('about.html')
+
+@app.route('/resultados')
+def results():
+    return render_template('results.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
