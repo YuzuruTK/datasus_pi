228e78a4d0134057cbc4717e4b372ac2de4b1ae9
Orfeu o Escarlate <darkarontg@gmail.com>
2024-06-21 21:34:48 -0300

    changes on database scripts and some testings on regression models

--- data_analysis/database_optimizations.sql
+++ data_analysis/database_optimizations.sql
@@ -1,1 +1,12 @@
 CREATE INDEX
+
+CREATE TABLE time (
+    special_key VARCHAR PRIMARY KEY,
+    year INTEGER,
+    quarter INTEGER,
+    month INTEGER,
+    month_name VARCHAR,
+);
+
+DROP VIEW IF EXISTS view_bizarra;
+CREATE VIEW 
